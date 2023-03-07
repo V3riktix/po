2144ab9a50ee15460cdd3c113d02f270f9f20c0f
dsnt <dsnt@DESKTOP-GPC3HIM>
2023-03-07 23:09:00 +0100

    Add project files.

---
+++ PO_Lab01/Garage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_Lab01
+{
+    public class Garage
+    {
+        private Car[] _cars;
+        private string _address;
+        private int _carsCount = 0;
+        private int _capacity;
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set { 
+                _capacity = value;
+                _cars = new Car[_capacity];
+            }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        public Garage()
+        {
+            Address = "Nieznany";
+            _cars = null;
+            Capacity = 0;
+        }
+        public Garage(string address, int capacity)
+        {
+            Address = address;
+            Capacity = capacity;
+        }
+        public void CarIn(Car car)
+        {
+            if(_carsCount == _capacity)
+            {
+                Console.WriteLine("Garaż pełny");
+            }
+            else
+            {
+                _carsCount++;
+                _cars[_carsCount - 1] = car;
+            }
+
+        }
+        public Car CarOut() {
+            if(_carsCount == 0)
+            {
+                Console.WriteLine("Garaż Pusty");
+                return null;
+            }
+            else
+            {
+                _carsCount--;
+                return _cars[_carsCount];
+            }
+        }
+        public override string ToString()
+        {
+            string s = $"Garage | Address: {_address}, Capacity: {_capacity}\n";
+            foreach(Car car in _cars)
+            {
+                
+                s += $"\t{car.ToString()}\n";
+            }
+            return s;
+        }
+        public void Details()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ PO_Lab01/Person.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_Lab01
+{
+    public class Person
+    {
+        private string[] _registarionNumbers = new string[3];
+        private string _firstName;
+        private string _lastName;
+        private string _address;
+        private int _carsCount;
+        private static int _maxCarCount = 3;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        public int CarsCount
+        {
+            get { return _carsCount; }
+            set {
+                if (value > MaxCarCount)
+                {
+                    _carsCount = MaxCarCount;
+                }
+                else
+                {
+                    _carsCount = value;
+                }
+            }
+        }
+        public static int MaxCarCount
+        {
+            get { return _maxCarCount; }
+        }
+
+        public Person()
+        {
+            _registarionNumbers = null;
+            _firstName = "Nieznane";
+            _lastName = "Nieznane";
+            _address = "Nieznany";
+            _carsCount = 0;
+        }
+        public Person(string firstName, string lastName, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            _registarionNumbers = null;
+            _carsCount = 0;
+        }
+        public Person(string firstName, string lastName, string address, Car[] cars)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            CarsCount = cars.Length;
+            for(int i = 0; i < CarsCount; ++i) {
+                _registarionNumbers[i] = cars[i].RegistrationNumber;
+            }
+        }
+        public void AddCarRegistrationNumber(string registrationNumber)
+        {
+            if (CarsCount == MaxCarCount) {
+                Console.WriteLine("Nie można dodać więcej aut");
+            }
+            else
+            {
+                _registarionNumbers[CarsCount] = registrationNumber;
+                CarsCount++;
+            }
+        }
+        public void RemoveCarRegistrationNumber(string registrationNumber)
+        {
+            if(CarsCount == 0)
+            {
+                Console.WriteLine("Nie można usunąć aut: brak pojazdów");
+            }
+            else
+            {
+                CarsCount--;
+            }
+        }
+        public override string ToString()
+        {
+            string s = $"Person | Name: {FirstName}, LastName: {LastName}, Address: {Address}\n Registration Numbers: ";
+            for(int i = 0; i < CarsCount; ++i)
+            {
+                s += $"{_registarionNumbers[i]}";
+            }
+            return s;
+        }
+        public void Details()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
